27ce43a1b874318f5a2fee25a9f80bc993f2f769
Qiizoff <qiizoff@gmail.com>
2020-07-15 00:51:59 +0300

    Update brut2.py

--- python/brut2.py
+++ python/brut2.py
@@ -3,8 +3,8 @@ from numpy import base_repr
 s = 'td78nv'
 
 for i in range(int(s, 36), 0, -1):
-    print(i)
+    # print(i)
     p = (base_repr(i, 36).lower())
     print(p)
-    if i == 1775687971:
-        break
+    # if i == 1775687971:
+    #     break
