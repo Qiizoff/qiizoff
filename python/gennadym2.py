72fbc6919eba484765d482d5ec88c1f6c4f3dcb3
Qiizoff <qiizoff@gmail.com>
2020-08-26 02:20:12 +0300

    123

--- python/gennadym2.py
+++ python/gennadym2.py
@@ -4,15 +4,15 @@ n = 0
 while n <= 99:
     n += 1
     # print('\nЦикл:', n)
-    b = 100
+    b = 1000
     i = 0
     while i <= 99:
         i += 1
         y = random.randint(0, 1)
         if y == 1:
-            b = b+50
+            b = b*1.05
         else:
-            b = b-50
+            b = b*0.975
     mylist.append(b)
 mylist2 = (sorted(mylist))
 for x in mylist2:
--- python/gennadym3.py
+++ python/gennadym3.py
@@ -3,15 +3,15 @@ mylist = list()
 n = 0
 while n <= 9:
     n += 1
-    b = 100
+    b = 100*10
     i = 0
-    while i <= 9:
+    while i <= 99:
         i += 1
         y = random.randint(0, 1)
         if y == 1:
-            b = b+50
+            b = b*1.05
             print('+', '%.5f' % b)
         else:
-            b = b-50
+            b = b*0.975
             print('-', '%.5f' % b)
     print('\nБаланс:', n, '=', '%.1f' % b)
