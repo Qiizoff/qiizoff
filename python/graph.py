8f41bf578ccb9de34fd1b191393d03765551a677
Qiizoff <qiizoff@gmail.com>
2020-09-25 18:04:35 +0300

    up

--- python/graph.py
+++ python/graph.py
@@ -1,7 +1,7 @@
 import plotly.graph_objects as go
 
 fig = go.Figure(
-    data=[go.Bar(x=[1, 2, 3], y=[1, 3, 2])],
+    data=[go.Bar(x=[1, 2, 3], y=[10, 3, 2])],
     layout=go.Layout(
         title=go.layout.Title(text="A Figure Specified By A Graph Object")
     )
