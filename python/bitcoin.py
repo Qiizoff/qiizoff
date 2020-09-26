7845915a661654ed6a344d952f603e9bb1ed60e7
Qiizoff <qiizoff@gmail.com>
2020-09-26 08:24:09 +0300

    Update bitcoin.py

--- python/bitcoin.py
+++ python/bitcoin.py
@@ -1,30 +1,54 @@
 # # https://www.binance.vision/ru/halving
-
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
+#Старт сети
 start_btc = datetime(2009, 1, 9, 0, 5, 30)
 print("\nСтарт сети BTC:", start_btc)
+#Изначальный размер блока
 block = 50
 print("Размер блока:", block)
+
 time_btc = start_btc
 i = 0
-btc = 0
-first_h = 210000
-second_f = 0
-hardfork = 0
-while i <= first_h + second_f:
+btc = 0 #Кол-во BTC
+first_h = 210000 #Халвинг на блоке 210к
+second_h = 0 #Счетчик блоков кратный 210к 
+halving = 0 #Кол-во халвингов
+
+x = []
+y = []
+
+while i <= first_h + second_h:
     time_btc += timedelta(minutes=9, seconds=30)
     btc += block
-    if i == first_h + second_f:
-        hardfork += 1
-        print("\nХардфорк №:", hardfork)
+    if i == first_h + second_h:
+        halving += 1
+        push = x.append
+        push(halving)
+        print("\nХалвинг №:", halving)
         print("Дата:", time_btc)
         block = block/2
         print("Размер блока:", block)
         print("Блоков за время:", 210000*block)
         print("Эмиссия BTC:", btc)
-        second_f += 210000
-        print("Хардфорк на блоке:", second_f)
+        push = y.append
+        push(btc)
+        second_h += 210000
+        print("Халвинг на блоке:", second_h)
         if btc >= 21000000:
             break
     i += 1
+
+
+print('x:',x)
+print('y:',y)
+
+fig = go.Figure(
+    data=[go.Bar(x=x, y=y)],
+    layout=go.Layout(
+        title=go.layout.Title(text="BTC")
+    )
+)
+
+fig.show()
