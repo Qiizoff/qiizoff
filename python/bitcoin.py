a1e4e25bd33f7dca838e6b7a035f8352d7174cd8
Qiizoff <qiizoff@gmail.com>
2020-09-26 08:52:41 +0300

    Update bitcoin.py

--- python/bitcoin.py
+++ python/bitcoin.py
@@ -3,13 +3,14 @@ import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
 # Старт сети
-start_btc = datetime(2009, 1, 9, 0, 5, 30)
-print("\nСтарт сети BTC:", start_btc)
+time = datetime(2009, 1, 9, 0, 5, 30)
+print("\nСтарт сети BTC:", time)
+
 # Изначальный размер блока
 block = 50
 print("Размер блока:", block)
 
-time_btc = start_btc
+
 i = 0
 btc = 0  # Кол-во BTC
 first_h = 210000  # Халвинг на блоке 210к
@@ -20,29 +21,28 @@ x = []
 y = []
 
 while i <= first_h + second_h:
-    time_btc += timedelta(minutes=9, seconds=30)
+    time += timedelta(minutes=9, seconds=30)
     btc += block
     if i == first_h + second_h:
         second_h += 210000
         halving += 1
         push = x.append
         push(halving)
+        print('x:', x)
         print("\nХалвинг №:", halving)
-        print("Дата:", time_btc)
+        print("Дата:", time)
         block = block/2
         print("Размер блока:", block)
         print("Блоков за время:", 210000*block)
         print("Эмиссия BTC:", btc)
         push = y.append
-        push(btc)
+        push(round(btc))
+        print('y:', y)
         print("Халвинг на блоке:", second_h)
         if btc >= 21000000:
             break
     i += 1
 
-
-print('x:', x)
-print('y:', y)
 
 fig = go.Figure(
     data=[go.Bar(x=x, y=y)],
